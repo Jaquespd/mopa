3d59c5b6d88964b663ccee4495859d1c2e32f696
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-29 19:52:48 +0100

    Cities Seeds and fixed cities/car's Migrations

--- backend/src/database/migrations/20200529170539-create-cities.js
+++ backend/src/database/migrations/20200529170539-create-cities.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable('cities', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -24,6 +24,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('cities');
   },
 };
--- backend/src/database/migrations/20200529172159-create-cars.js
+++ backend/src/database/migrations/20200529172159-create-cars.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('service_problems', {
+    return queryInterface.createTable('cars', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -27,6 +27,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('service_problems');
+    return queryInterface.dropTable('cars');
   },
 };
