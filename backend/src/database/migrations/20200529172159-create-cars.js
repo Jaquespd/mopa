df326783c3666cb9a77d57ca5527e7d2115f58af
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-29 18:27:01 +0100

    Car's Migration

---
+++ backend/src/database/migrations/20200529172159-create-cars.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('service_problems', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      car_model: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      license_plate: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('service_problems');
+  },
+};
