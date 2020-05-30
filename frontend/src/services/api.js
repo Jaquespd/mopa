e25faddac4487095fc5d658cd0b3b666543610f9
Jaques <jaquespd@gmail.com>
2020-05-30 14:20:17 -0300

    settings for deploy netilify - 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   // Development
   // baseURL: 'http://localhost:3333',
   // Production
-  baseURL: 'http://178.128.153.78',
+  baseURL: 'https://178.128.153.78',
 });
 
 export default api;
