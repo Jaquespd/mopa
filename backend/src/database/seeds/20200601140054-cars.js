766220393e41d6bb91eee88015ce39d187486839
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-06-01 15:23:13 +0100

    new seeds (cars)

--- backend/src/database/seeds/20200601140054-cars.js
+++ backend/src/database/seeds/20200601140054-cars.js
@@ -1,30 +1,40 @@
 module.exports = {
   up: QueryInterface => {
     return QueryInterface.bulkInsert(
-      'cities',
+      'cars',
       [
         {
-          city: 'Mossoró',
+          name: 'Carro 01',
+          model: 'Fusca',
+          license_plate: 'MPRN 0001',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          city: 'Monte Alegre',
+          name: 'Carro 02',
+          model: 'Belina',
+          license_plate: 'MPRN 0002',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          city: 'Goianinha',
+          name: 'Carro 03',
+          model: 'Fiat 147',
+          license_plate: 'MPRN 0003',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          city: 'Touros',
+          name: 'Carro 04',
+          model: 'Bora',
+          license_plate: 'MPRN 0004',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          city: 'Macaíba',
+          name: 'Carro 05',
+          model: 'Opala',
+          license_plate: 'MPRN 0005',
           created_at: new Date(),
           updated_at: new Date(),
         },
