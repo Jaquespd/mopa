cbca878a0304f1c433c00fefa96632e3ba5fd982
Jaques <jaquespd@gmail.com>
2020-07-18 10:46:09 -0300

    Frontend Bug fix - change for deploy

---
+++ frontend/build/precache-manifest.6f7233e0fee7464651f749e01c002989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d089d8fd98ccc881d349afbad396098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9ebb56b639524523de",
+    "url": "/static/css/2.4949f480.chunk.css"
+  },
+  {
+    "revision": "dd9ebb56b639524523de",
+    "url": "/static/js/2.3be9de50.chunk.js"
+  },
+  {
+    "revision": "0fe92e4331dbced28d5736c51e9ea1c7",
+    "url": "/static/js/2.3be9de50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbb53fd07c610d17f53",
+    "url": "/static/js/main.5f0f65ef.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
