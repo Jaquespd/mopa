d6ddb771f7d9769f0f9239d579d117481e38f63c
Jaques <jaquespd@gmail.com>
2020-07-28 12:05:27 -0300

    Change address from api in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   // Development
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
   // Production
-  // baseURL: 'http://178.128.153.78',
+  baseURL: 'http://178.128.153.78:3333',
 });
 
 export default api;
