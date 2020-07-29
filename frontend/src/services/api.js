3c23a8b386e093d19531d808958c422fbe1c47c7
Jaques <jaquespd@gmail.com>
2020-07-29 09:11:27 -0300

    change api from frontend for https

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   // Development
   // baseURL: 'http://localhost:3333',
   // Production
-  baseURL: 'http://178.128.153.78:3333',
+  baseURL: 'https://178.128.153.78:3333',
 });
 
 export default api;
